0b4a1d8a42e684995e30409b7b4b223874462df4
Anush DP <anushdp.dsce@gmail.com>
2021-07-30 00:01:10 +0530

    updated server js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-var path = require("path");
-var express = require("express");
+var path = require('path');
+var express = require('express');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, "dist")));
-app.set("port", process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get("port"), function () {
-  console.log("listening on port ", server.address().port);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+var server = app.listen(app.get('port'), function () {
+  console.log('listening on port ', server.address().port);
 });
